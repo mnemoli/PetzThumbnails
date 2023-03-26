986b5a25ea621e641aae20944a50f3c52d62501c
mnemoli <mnemoli@users.noreply.github.com>
2023-03-26 15:20:34 +0100

    Add files via upload

---
+++ PetzThumbnailHandler.cs
@@ -0,0 +1,27 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpThumbnailHandler;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PetzThumbnails
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.FileExtension, ".pet")]
+
+    public class PetzThumbnailHandler : SharpThumbnailHandler
+    {
+        protected override Bitmap GetThumbnailImage(uint width)
+        {
+            var headerBytes = new byte[] { 0x42, 0x4d, 0xf6, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36, 0x04, 0x00, 0x00 };
+            SelectedItemStream.Seek(-13704, SeekOrigin.End);
+            using (var mem = new MemoryStream()) 
+            {
+                mem.Write(headerBytes, 0, headerBytes.Length);
+                SelectedItemStream.CopyTo(mem);
+                return new Bitmap(mem);
+
+            }
+        }
+    }
+}
